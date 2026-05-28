cc0f0bf7435854ef868ceb63542fff19a3ddea2d
Peether-PTDT <pinkpeether@gmail.com>
2026-05-28 09:53:35 +0500

    Add permission review to diagnostics route

--- src/routes/supportDiagnostics.routes.ts
+++ src/routes/supportDiagnostics.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { authenticate, authorize } from '../middleware/auth'
 import * as SupportDiagnosticsController from '../controllers/supportDiagnostics.controller'
+import * as PermissionReviewController from '../controllers/permissionReview.controller'
 
 const router = Router()
 
@@ -9,5 +10,6 @@ router.use(authorize('ADMIN', 'SUPERVISOR'))
 
 router.get('/', SupportDiagnosticsController.getDiagnostics)
 router.get('/download', SupportDiagnosticsController.downloadDiagnostics)
+router.get('/access-review', authorize('ADMIN'), PermissionReviewController.getReview)
 
 export default router
